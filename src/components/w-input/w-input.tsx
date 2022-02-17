cbd4c9209a745efe7bcfcf0769389a2d7afe0235
Wesley <wes.urb@gmail.com>
2022-02-17 14:58:30 +0100

    add slot and set attributes on request

--- src/components/w-input/w-input.tsx
+++ src/components/w-input/w-input.tsx
@@ -15,7 +15,7 @@ export class WInput implements ComponentInterface {
   /**
    * the value of the input
    */
-  @Prop({ mutable: true, reflect: true }) value: string | number | null = '';
+  @Prop({ mutable: true }) value: string | number | null = '';
   @Watch('value')
   protected valueChanged() {
     this.wChange.emit(this.value);
@@ -65,6 +65,11 @@ export class WInput implements ComponentInterface {
   @Prop() design?: 'primary' | 'error' | 'success' | 'warning' | 'secondary' = 'secondary';
 
   /**
+   * decide if (and which site) a slot should be rendered
+   */
+  @Prop() showSlot?: 'left' | 'right' | 'both';
+
+  /**
    * show error message below the input
    */
   @Prop() errorMsg?: string = '';
@@ -110,22 +115,22 @@ export class WInput implements ComponentInterface {
    * possible types:
    * https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/autocomplete
    */
-  @Prop() autoComplete?: string = 'off';
+  @Prop() autoComplete?: string;
 
   /**
    * decide if autocorect should be enabled
    */
-  @Prop() autoCorrect?: 'off' | 'on' = 'off';
+  @Prop() autoCorrect?: 'off' | 'on';
 
   /**
    * decide if autocapitalize should be enabled
    */
-  @Prop() autoCapitalize?: 'off' | 'none' | 'on' | 'sentences' | 'words' | 'characters' = 'off';
+  @Prop() autoCapitalize?: 'off' | 'none' | 'on' | 'sentences' | 'words' | 'characters';
 
   /**
    * decide if spellcheck should be enabled
    */
-  @Prop() spellCheck?: boolean = false;
+  @Prop() spellCheck?: boolean;
 
   /**
    * set the pattern for the input
@@ -140,7 +145,7 @@ export class WInput implements ComponentInterface {
   /**
    * emitted on change
    */
-  @Event() wChange: EventEmitter<number | string>;
+  @Event({ bubbles: false }) wChange: EventEmitter<number | string>;
 
   /**
    * emitted on focus
@@ -229,35 +234,52 @@ export class WInput implements ComponentInterface {
       this.inputElement.value = '';
     }
   };
+  private setAttr(): void {
+    if (!this.inputElement) return;
+    if (!!this.min) this.inputElement.setAttribute('min', this.min);
+    if (!!this.max) this.inputElement.setAttribute('max', this.max);
+    if (!!this.step) this.inputElement.setAttribute('step', this.step);
+    if (!!this.autoComplete) this.inputElement.setAttribute('autocomplete', this.autoComplete);
+    if (!!this.autoCorrect) this.inputElement.setAttribute('autocorrect', this.autoCorrect);
+    if (!!this.autoCapitalize) this.inputElement.setAttribute('autocapitalize', this.autoCapitalize);
+    if (!!this.pattern) this.inputElement.setAttribute('pattern', this.pattern);
+    if (this.spellCheck) this.inputElement.setAttribute('spellcheck', this.spellCheck.toString());
+    if (this.required) this.inputElement.setAttribute('required', this.required.toString());
+    if (this.disabled) this.inputElement.setAttribute('disabled', this.disabled.toString());
+    if (this.readonly) this.inputElement.setAttribute('readonly', this.readonly.toString());
+    // autoFocus={false}
+  }
+
+  componentDidLoad(): void {
+    this.setAttr();
+  }
 
   render() {
     const value = this.getValue();
     return (
       <Host>
+        {this.showSlot === 'left' && <slot></slot>}
+        {this.showSlot === 'both' && <slot name="left"></slot>}
         <div
           onMouseEnter={() => (this.hovered = true)}
           onMouseLeave={() => (this.hovered = false)}
+          onClick={() => this.inputElement.focus()}
           class={{
             wrapper: true,
             disabled: this.disabled,
-            // sm: this.size == 'small',
-            // md: this.size == 'medium',
-            // lg: this.size == 'large',
             [this.design]: true,
             [this.size]: true,
             errorText: !this.validate && !!!this.errorMsg,
+            [`slot-${this.showSlot}`]: !!this.showSlot,
           }}
         >
           <input
             class={{
               input: true,
               [`text-${this.size}`]: true,
-              [`padding-${this.size}`]: this.hovered || this.focus || !!this.value,
-              // [this.design]: true,
+              // [`padding-${this.size}`]: this.hovered || this.focus || !!this.value,
             }}
             ref={input => (this.inputElement = input)}
-            min={this.min}
-            max={this.max}
             type={this.inputType}
             value={value}
             onInput={this.onInput}
@@ -265,24 +287,12 @@ export class WInput implements ComponentInterface {
             onClick={this.onClick}
             onFocus={this.onFocus}
             onKeyDown={this.onKeyDown}
-            pattern={this.pattern}
-            required={this.required}
-            readonly={this.readonly}
-            disabled={this.disabled}
-            autoComplete={this.autoComplete}
-            autoCapitalize={this.autoCapitalize}
-            autoCorrect={this.autoCorrect}
-            spellcheck={this.spellCheck}
-            autoFocus={false}
             placeholder={this.label}
           />
           {!!this.label ? (
             <label
               class={{
                 label: true,
-                // smLabel: this.size == 'small',
-                // mdLabel: this.size == 'medium',
-                // lgLabel: this.size == 'large',
                 [this.design]: true,
                 [`label-${this.size}`]: true,
               }}
@@ -290,7 +300,7 @@ export class WInput implements ComponentInterface {
               {this.required ? this.label + ' *' : this.label}
             </label>
           ) : null}
-          {!this.readonly && !this.disabled && this.value ? (
+          {!this.readonly && !this.disabled && this.value && (
             <div
               onClick={this.resetValue}
               onTouchStart={this.resetValue}
@@ -298,7 +308,7 @@ export class WInput implements ComponentInterface {
                 reset: true,
               }}
             >
-              {this.focus || this.hovered || this.value ? (
+              {(this.focus || this.hovered || this.value) && (
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width={this.size == 'small' ? 16 : this.size == 'medium' ? 20 : 24}
@@ -313,11 +323,13 @@ export class WInput implements ComponentInterface {
                   <path d="M15 15L9 9m6 0l-6 6" class={this.focus || this.hovered ? 'text-red-500' : 'text-gray-500'} />
                   <circle cx="12" cy="12" r="10" class={this.focus || this.hovered ? 'text-red-500' : 'text-gray-500'} />
                 </svg>
-              ) : null}
+              )}
             </div>
-          ) : null}
+          )}
           {!this.validate ? <span class={{ 'help-text': true }}>{this.errorMsg}</span> : null}
         </div>
+        {this.showSlot === 'right' && <slot></slot>}
+        {this.showSlot === 'both' && <slot name="right"></slot>}
       </Host>
     );
   }
