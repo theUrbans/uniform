d0818fff2ec2f07d7d6d86b211e3df6d9ed42025
Wesley <wes.urb@gmail.com>
2022-03-15 08:42:42 +0100

    first version

--- src/components/u-paragraph/u-paragraph.tsx
+++ src/components/u-paragraph/u-paragraph.tsx
@@ -1,7 +1,8 @@
-import { Component, Host, h } from '@stencil/core';
+import { Component, h, Prop } from '@stencil/core';
 
 /**
  * @name Paragraph
+ * @state 🟡
  */
 @Component({
   tag: 'u-paragraph',
@@ -9,11 +10,27 @@ import { Component, Host, h } from '@stencil/core';
   shadow: true,
 })
 export class UParagraph {
+  @Prop() color: string;
+  @Prop() backgroundColor: string;
+  @Prop() hyphens: string;
+  @Prop() textAlign: 'jusitfy' | 'center' | 'left' | 'right';
+  @Prop() textAlignLast: 'jusitfy' | 'center' | 'left' | 'right' | 'none' = 'none';
   render() {
     return (
-      <Host>
-        <slot></slot>
-      </Host>
+      <p
+        style={{
+          color: this.color,
+          backgroundColor: this.backgroundColor,
+          hyphens: this.hyphens,
+          textAlign: this.textAlign,
+          textAlignLast: this.textAlignLast,
+        }}
+      >
+        <slot>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas harum animi amet quas perspiciatis? Dolores, tempore necessitatibus impedit deleniti laudantium facere
+          facilis, sint quo, minus velit sit quibusdam hic. Rem?
+        </slot>
+      </p>
     );
   }
 }
