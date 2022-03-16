57c20052c7d7708a749397118325e0c06f3e78cd
Wesley <wes.urb@gmail.com>
2022-03-16 13:51:11 +0100

    first edit

--- src/components/u-list/u-list.tsx
+++ src/components/u-list/u-list.tsx
@@ -6,14 +6,15 @@ import { Component, Host, h } from '@stencil/core';
 @Component({
   tag: 'u-list',
   styleUrl: 'u-list.css',
-  shadow: true,
 })
 export class UList {
   render() {
     return (
-      <Host>
-        <slot></slot>
-      </Host>
+      <Host
+        class={{
+          list: true,
+        }}
+      ></Host>
     );
   }
 }
