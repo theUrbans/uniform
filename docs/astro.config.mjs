527cde1f0bb128cfeb63fb3c6ffd9f9aa45876c7
Wesley <wes.urb@gmail.com>
2022-05-03 17:06:52 +0200

    add base path

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -7,6 +7,7 @@ import vue from '@astrojs/vue';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://theurbans.github.io',
+  base: '/uniform',
   integrations: [
     // Enable Preact to support Preact JSX components.
     preact(), // Enable React for the Algolia search component.
