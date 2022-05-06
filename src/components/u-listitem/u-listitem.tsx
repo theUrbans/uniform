cf7b97820bc33ab0b9fab8768e47fd9ab69ad069
Wesley <wes.urb@gmail.com>
2022-05-06 09:50:44 +0200

    first barebone version of lists

--- src/components/u-listitem/u-listitem.tsx
+++ src/components/u-listitem/u-listitem.tsx
@@ -1,16 +1,21 @@
-import { Component, Host, h } from '@stencil/core';
+import { Component, h } from '@stencil/core';
 
+/**
+ * @name List item
+ * @state 🟡
+ * @description single list item
+ * @slot default - item content
+ */
 @Component({
   tag: 'u-listitem',
-  styleUrl: 'u-listitem.css',
-  shadow: true
+  styleUrl: 'u-listitem.css'
 })
 export class UListitem {
   render() {
     return (
-      <Host>
+      <li>
         <slot></slot>
-      </Host>
+      </li>
     );
   }
 }
