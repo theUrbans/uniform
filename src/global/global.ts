f046d240ba484fe72e774be74887f628f63353d4
Wesley <wes.urb@gmail.com>
2022-05-12 08:34:42 +0200

    update mode detection

--- src/global/global.ts
+++ src/global/global.ts
@@ -2,19 +2,25 @@ import { setMode } from '@stencil/core';
 
 declare const Context: any;
 try {
-  const userAgent =
-    navigator.userAgent || navigator.vendor || (<any>window).opera;
-  if (/windows phone/i.test(userAgent)) {
-    Context.mode = 'mobile';
-  } else if (/android/i.test(userAgent)) {
-    Context.mode = 'mobile';
-  } else if (/iPad|iPhone|iPod/.test(userAgent)) {
-    Context.mode = 'mobile';
-  } else {
-    Context.mode = 'desktop';
-  }
-
-  document.documentElement.setAttribute('mode', Context.mode);
+  // const userAgent =
+  //   navigator.userAgent || navigator.vendor || (<any>window).opera;
+  // if (/windows phone/i.test(userAgent)) {
+  //   Context.mode = 'mobile';
+  // } else if (/android/i.test(userAgent)) {
+  //   Context.mode = 'mobile';
+  // } else if (/iPad|iPhone|iPod/.test(userAgent)) {
+  //   Context.mode = 'tablet';
+  // } else {
+  //   Context.mode = 'desktop';
+  // }
+  const mode =
+    window.innerWidth <= 768
+      ? 'mobile'
+      : window.innerWidth <= 992
+      ? 'tablet'
+      : 'desktop';
+  Context.mode = mode;
+  document.documentElement.setAttribute('mode', mode);
 } catch (error) {
   Context.mode = 'desktop';
 }
